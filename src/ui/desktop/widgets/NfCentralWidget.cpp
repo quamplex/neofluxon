6b6d8b1302c41b60c6341f637bfa4ede1360900c
Iurie Nistor <iuriehn@gmail.com>
2026-04-08 15:29:00 +0300

    central widget

---
+++ src/ui/desktop/widgets/NfCentralWidget.cpp
@@ -0,0 +1,84 @@
+/**
+ * File name: NfCentralWidget.h
+ * Project: Neofluxon (a photography workflow software)
+ *
+ * Copyright (C) 2026 Iurie Nistor
+ *
+ * This file is part of Neofluxon.
+ *
+ * Neofluxon is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "NfCentralWidget.h"
+
+#include "GridView.h"
+#include "PreviewView.h"
+
+#include <QVBoxLayout>
+
+NfCentralWidget::NfCentralWidget(QWidget* parent)
+        : QWidget(parent)
+        , m_centralStack{new QStackedWidget(this)}
+{
+        auto* layout = new QVBoxLayout(this);
+        layout->setContentsMargins(0, 0, 0, 0);
+        layout->addWidget(m_centralStack);
+
+        setLayout(layout);
+
+        setupConnections();
+}
+
+NfGridView* NfCentralWidget::gridView()
+{
+        if (!m_gridView) {
+                m_gridView = new GridView(this);
+                m_stack->addWidget(m_gridView);
+
+                connect(m_gridView, &GridView::imageActivated,
+                        this, &NfCentralWidget::showPreview);
+        }
+        return m_gridView;
+}
+
+NfPreviewView* NfCentralWidget::previewView()
+{
+        if (!m_previewView) {
+                m_previewView = new PreviewView(this);
+                m_stack->addWidget(m_previewView);
+
+                connect(m_previewView, &PreviewView::exitRequested,
+                        this, &NfCentralWidget::showGrid);
+
+                connect(m_previewView, &PreviewView::requestImage,
+                        this, &NfCentralWidget::showPreview);
+        }
+        return m_previewView;
+}
+
+void NfCentralWidget::showGrid()
+{
+        auto* view = gridView();
+        m_stack->setCurrentWidget(view);
+        emit gridRequested();
+}
+
+void NfCentralWidget::showPreview(int index)
+{
+        auto* view = previewView();
+        view->setCurrentIndex(index);
+        m_stack->setCurrentWidget(view);
+        emit previewRequested(index);
+}
---
+++ src/ui/desktop/widgets/NfCentralWidget.h
@@ -0,0 +1,63 @@
+/**
+ * File name: NfCentralWidget.h
+ * Project: Neofluxon (a photography workflow software)
+ *
+ * Copyright (C) 2026 Iurie Nistor
+ *
+ * This file is part of Neofluxon.
+ *
+ * Neofluxon is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef NF_CENTRAL_WIDGET_H
+#define NF_CENTRAL_WIDGET_H
+
+#include <QWidget>
+#include <QStackedWidget>
+
+namespace NfDesktop {
+
+class NfGridView;
+class NfPreviewView;
+class NfEditView;
+class QStackedWidget;
+
+class NfCentralWidget : public QWidget
+{
+        Q_OBJECT
+
+ public:
+        explicit NfCentralWidget(QWidget* parent = nullptr);
+
+ public slots:
+        void showGridMode();
+        void showPreviewMode();
+        void showEditMode();
+
+ signals:
+        void gridModeRequested();
+        void previewModeRequested();
+        void editModeRequested();
+
+ private:
+        QStackedWidget* m_centralStack;
+        NfGridView* m_gridView;
+        NfPreviewView* m_previewView;
+        NfEditView* m_editView;
+};
+
+} // namespace NfDesktop
+
+#endif // NF_CENTRAL_WIDGET_H
